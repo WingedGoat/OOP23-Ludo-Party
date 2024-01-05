078c9530cbec972613c907bf4ff538274d65996d
Colavita Ilaria <ilaria.colavita@studio.unibo.it>
2024-01-05 19:39:25 +0100

    Add Game class

---
+++ src/main/java/model/Game.java
@@ -0,0 +1,22 @@
+package model;
+
+/**
+ * 
+ */
+@SuppressWarnings("PMD")
+public final class Game {
+
+    private static final int PAWN_NUMBER = 4;
+
+    private Game() {  }
+
+    /**
+     * Get pawn number.
+     * @return PAWN_NUMBER.
+     */
+    public static int getPawnNumber() {
+        return PAWN_NUMBER;
+    }
+
+
+}
