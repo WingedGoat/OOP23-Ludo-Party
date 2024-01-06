30643f6684628e72ffd3791dd88da8c83c34becc
Colavita Ilaria <ilaria.colavita@studio.unibo.it>
2024-01-06 23:29:15 +0100

    Add hashCode and equals methods to class Position

--- src/main/java/utility/Position.java
+++ src/main/java/utility/Position.java
@@ -39,6 +39,30 @@ public final class Position {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Position other = (Position) obj;
+        return x == other.x;
+    }
+
+    @Override
     public String toString() {
         return "Pair [x=" + x + ", y=" + y + "]";
     }
