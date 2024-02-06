ee6a8fc362cfef440cde2e69755907a9463290e7
Colavita Ilaria <ilaria.colavita@studio.unibo.it>
2024-02-06 14:11:34 +0100

    Add custom spotbugs rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,11 @@ repositories {
     mavenCentral()
 }
 
+spotbugs {
+    ignoreFailures.set(false)
+    excludeFilter = file("spotbugs-exclude.xml")
+}
+
 val javaFXModules = listOf("base","controls","fxml","swing","graphics")
 val supportedPlatforms = listOf("linux","mac","win") 
 val javaFxVersion = 17
