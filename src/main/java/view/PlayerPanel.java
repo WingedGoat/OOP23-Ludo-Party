fc8fe9ba333e17131a3debf0097bd458aacf7863
Colavita Ilaria <ilaria.colavita@studio.unibo.it>
2024-02-11 18:38:22 +0100

    Refactor of PlayerPanel with Template Method Pattern

--- src/main/java/view/PlayerPanel.java
+++ src/main/java/view/PlayerPanel.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.nio.file.Path;
 
 import javafx.scene.Cursor;
+import javafx.scene.Group;
 import javafx.scene.control.Label;
 import javafx.scene.effect.Glow;
 import javafx.scene.image.Image;
@@ -11,91 +12,117 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+
+import controller.api.Controller;
 import model.Position;
 /**
- * Player panel basic class.
+ * Abstract Player panel class.
  */
-public class PlayerPanel extends AnchorPane {
+public abstract class PlayerPanel extends AnchorPane {
 
     /**
-     * image path.
+     * dice image folder path.
      */
-    protected static final String DICE_IMAGE_FOLDER = Path.of("." + ViewUtility.FILE_SEPARATOR 
+    private static final String DICE_IMAGE_FOLDER = Path.of("." + ViewUtility.FILE_SEPARATOR 
         + "resources" + ViewUtility.FILE_SEPARATOR
         + "images" + ViewUtility.FILE_SEPARATOR 
         + "dices").toString();
+
+    private static final int PANE_HEIGHT = 600;
+    private static final int CIRCLE_RADIUS = 80;
+    private static final int INNER_CIRCLE_RADIUS = 77;
+    private static final String CIRCLE_COLOR = "#555555";
+    private static final String INNER_CIRCLE_COLOR = "#F1F1F1";
+
+    private static final int LABEL_X_LAYOUT = 60;
+    private static final int LABEL_TOP_NAME_Y_LAYOUT = 230;
+    private static final int LABEL_TOP_COINS_Y_LAYOUT = 250;
+
+    private static final int AVATAR_X_POSITION = 110;
+    private static final int AVATAR_Y_TOP_POSITION = 120;
+    private static final int AVATAR_Y_BOTTOM_POSITION = 480;
+
+    private static final int DICE_WIDTH = 40;
+    private static final int DICE_HEIGHT = 40;
     /**
-     * pane height.
-     */
-    protected static final int PANE_HEIGHT = 600;
-    // circle props
-    /**
-     * circle radius.
-     */
-    protected static final int CIRCLE_RADIUS = 80;
-    /**
-     * inner circle radius.
-     */
-    protected static final int INNER_CIRCLE_RADIUS = 77;
-    /**
-     * circle color.
-     */
-    protected static final String CIRCLE_COLOR = "#555555";
-    /**
-     * inner circle color.
-     */
-    protected static final String INNER_CIRCLE_COLOR = "#F1F1F1";
-    // components positions
-    /**
-     * x.
-     */
-    protected static final int LABEL_X_LAYOUT = 60;
-    /**
-     * AA.
-     */
-    protected static final int LABEL_TOP_NAME_Y_LAYOUT = 230;
-    /**
-     * AA.
-     */
-    protected static final int LABEL_TOP_COINS_Y_LAYOUT = 250;
-    /**
-     * AA.
+     * label bottom name Y layout.
      */
     protected static final int LABEL_BOTTOM_NAME_Y_LAYOUT = 350;
     /**
-     * AA.
+     * label bottom coins Y layout.
      */
     protected static final int LABEL_BOTTOM_COINS_Y_LAYOUT = 370;
     /**
-     * AA.
+     * dice X layout.
      */
-    protected static final int DICE_WIDTH = 40;
+    protected static final int DICE_X_LAYOUT = 170;
     /**
-     * AA.
+     * dice Y layout at top position.
      */
-    protected static final int DICE_HEIGHT = 40;
-//    protected static final int DICE_X_LAYOUT = 120;
-//    protected static final int DICE_TOP_Y_LAYOUT = 210;
-//    protected static final int DICE_BOTTOM_Y_LAYOUT = 340;
+    protected static final int DICE_Y_LAYOUT_TOP = 260;
     /**
-     * AA.
+     * dice Y layout at bottm position.
      */
-    protected static final int CENTOSETTANTA = 170;
-    //private static final int DUEEVENTI = 220;
+    protected static final int DICE_Y_LAYOUT_BOTTOM = 340;
+
+    private final Position bottomPos;
+    private final Position topPos;
+
+    PlayerPanel() {
+        this.topPos = new Position(AVATAR_X_POSITION, AVATAR_Y_TOP_POSITION);
+        this.bottomPos = new Position(AVATAR_X_POSITION, AVATAR_Y_BOTTOM_POSITION);
+        this.setPrefHeight(PANE_HEIGHT);
+    }
+
     /**
-     * AA.
+     * Gets the bottom position of the player avatar.
+     * @return the bottom position
      */
-    protected static final int TREEQUARANTA = 340;
+    protected Position getBottomPos() {
+        return this.bottomPos;
+    }
+
     /**
-     * AA.
+     * Gets the top position of the player avatar.
+     * @return the bottom position
      */
-    protected static final int DUEESESSANTA = 260;
+    protected Position getTopPos() {
+        return this.topPos;
+    }
+
+    /**
+     * Creates the player Group at bottom corner.
+     * 
+     * @param pos the position of the player avatar
+     * @param ctrl the controller
+     * @return the player Group
+     */
+    protected abstract Group createBottomPlayer(Position pos, Controller ctrl);
+
+    /**
+     * Creates the player Group at top corner.
+     * 
+     * @param pos the position of the player avatar
+     * @param ctrl the controller
+     * @return the player Group
+     */
+    protected abstract Group createTopPlayer(Position pos, Controller ctrl);
+
+    /**
+     * Sets the node anchors.
+     * 
+     * @param label the node
+     * @param value the position
+     */
+    protected void setNodeAnchors(final Label label, final double value) {
+        AnchorPane.setLeftAnchor(label, value);
+        AnchorPane.setRightAnchor(label, value);
+    }
 
     /**
      * Creates the outer circle of the player avatar.
      * 
      * @param pos pos
-     * 
      * @return the player avatar
      */
     protected Circle createPlayerAvatar(final Position pos) {
@@ -106,7 +133,6 @@ public class PlayerPanel extends AnchorPane {
      * Creates the inner circle of the player avatar.
      * 
      * @param pos pos
-     * 
      * @return the player avatar
      */
     protected Circle createPlayerInnerAvatar(final Position pos) {
@@ -117,7 +143,6 @@ public class PlayerPanel extends AnchorPane {
      * Creates the label with the name of the player.
      * 
      * @param name the player name
-     * 
      * @return the label with the player name
      */
     protected Label createNameLabelTopPanel(final String name) {
@@ -132,10 +157,9 @@ public class PlayerPanel extends AnchorPane {
      * Creates the label with the coin quantity of the player.
      * 
      * @param coins the amount of coins owned by the player
-     * 
      * @return label with the coin amount
      */
-    protected Label createCoinLabelTopPanel(final int coins) {
+    protected Label createCoinsLabelTopPanel(final int coins) {
         final Label playerCoins = new Label("Ludollari: " + coins);
         playerCoins.setLayoutX(LABEL_X_LAYOUT);
         playerCoins.setLayoutY(LABEL_TOP_COINS_Y_LAYOUT);
--- src/main/java/view/PlayerPanelLeft.java
+++ src/main/java/view/PlayerPanelLeft.java
@@ -1,11 +1,11 @@
 package view;
 
-import controller.api.Controller;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
+
+import controller.api.Controller;
 import model.Position;
 
 /**
@@ -20,86 +20,65 @@ public final class PlayerPanelLeft extends PlayerPanel {
      */
     public PlayerPanelLeft(final Controller controller) {
 
-        this.setPrefHeight(PANE_HEIGHT);
-        final Position bottomPos = new Position(110, 480);
-        final Position topPos = new Position(110, 120);
-
-        final Group g1 = createBottomLeftPlayer(bottomPos, controller);
-        //final Group g2 = createTopRightPlayer(topPos, controller);
-
-        //add bottom LEFT group and top RIGHT group
+        final Group g1 = createBottomPlayer(this.getBottomPos(), controller);
         this.getChildren().add(g1);
 
         if (controller.getGame().getPlayers().size() > 2) {
-            //add also top RIGHT group
-            //and bottom LEFT group
-            final Group g2 = createTopLeftPlayer(topPos, controller);
+            final Group g2 = createTopPlayer(this.getTopPos(), controller);
             this.getChildren().add(g2);
         }
     }
 
-    // HUMAN PLAYER (in BOTTOM LEFT position) [g1]
-    private Group createBottomLeftPlayer(final Position pos, final Controller ctrl) {
-        final Group g = new Group();
+    @Override
+    protected Group createBottomPlayer(final Position pos, final Controller ctrl) {
 
         final Circle playerAvatar = createPlayerAvatar(pos);
         final Circle playerAvatarInner = createPlayerInnerAvatar(pos);
 
-        final Label playerName = new Label(ctrl.getGame().getHumanPlayer().getName());
-        playerName.setLayoutX(LABEL_X_LAYOUT);
-        playerName.setLayoutY(LABEL_BOTTOM_NAME_Y_LAYOUT);
+        final Label playerName = createNameLabelTopPanel(ctrl.getGame().getHumanPlayer().getName());
+        setNodeAnchors(playerName, 0.0);
 
-        final Label playerCoins = new Label("Ludollari: " + ctrl.getGame().getHumanPlayer().getCoins());
-        playerCoins.setLayoutX(LABEL_X_LAYOUT);
-        playerCoins.setLayoutY(LABEL_BOTTOM_COINS_Y_LAYOUT);
+        final Label playerCoins = createCoinsLabelTopPanel(ctrl.getGame().getHumanPlayer().getCoins());
+        setNodeAnchors(playerCoins, 0.0);
 
-        AnchorPane.setLeftAnchor(playerName, 0.0);
-        AnchorPane.setRightAnchor(playerName, 0.0);
-
-        AnchorPane.setLeftAnchor(playerCoins, 0.0);
-        AnchorPane.setRightAnchor(playerCoins, 0.0);
-
-        // dice
         final ImageView diceImage = createDicImageView();
-        diceImage.setLayoutX(CENTOSETTANTA);
-        diceImage.setLayoutY(TREEQUARANTA);
+        diceImage.setLayoutX(DICE_X_LAYOUT);
+        diceImage.setLayoutY(DICE_Y_LAYOUT_BOTTOM);
 
+        //TODO
         diceImage.setOnMouseClicked(mouseEvent -> {
             final int diceResult = ctrl.getGame().getTurn().getCurrentPlayer().rollDice();
             showDiceNumber(diceImage, diceResult);
-            //System.out.println(diceResult);
         });
 
+        final Group g = new Group();
         g.getChildren().addAll(playerAvatar, playerAvatarInner, playerName, playerCoins, diceImage);
 
         return g;
     }
 
-    // COMPUTER PLAYER (in RIGHT TOP position) [g3]
-    private Group createTopLeftPlayer(final Position pos, final Controller ctrl) {
-        final Group g = new Group();
+    @Override
+    protected Group createTopPlayer(final Position pos, final Controller ctrl) {
 
         final Circle playerAvatar = createPlayerAvatar(pos);
         final Circle playerAvatarInner = createPlayerInnerAvatar(pos);
 
         final Label playerName = createNameLabelTopPanel(ctrl.getGame().getPlayers().get(1).getName());
-        final Label playerCoins = createCoinLabelTopPanel(ctrl.getGame().getPlayers().get(1).getCoins());
+        setNodeAnchors(playerName, 0.0);
 
-        AnchorPane.setLeftAnchor(playerName, 0.0);
-        AnchorPane.setRightAnchor(playerName, 0.0);
+        final Label playerCoins = createCoinsLabelTopPanel(ctrl.getGame().getPlayers().get(1).getCoins());
+        setNodeAnchors(playerCoins, 0.0);
 
-        AnchorPane.setLeftAnchor(playerCoins, 0.0);
-        AnchorPane.setRightAnchor(playerCoins, 0.0);
-
-        // dice
         final ImageView diceImage = createDicImageView();
-        diceImage.setLayoutX(CENTOSETTANTA);
-        diceImage.setLayoutY(DUEESESSANTA);
+        diceImage.setLayoutX(DICE_X_LAYOUT);
+        diceImage.setLayoutY(DICE_Y_LAYOUT_TOP);
 
+        //TODO
         diceImage.setOnMouseClicked(mouseEvent -> {
             // controller.get
         });
 
+        final Group g = new Group();
         g.getChildren().addAll(playerAvatar, playerAvatarInner, playerName, playerCoins, diceImage);
 
         return g;
