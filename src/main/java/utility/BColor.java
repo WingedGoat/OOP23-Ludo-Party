22e922613ac4fba143666fc9d9a852755ff3face
Colavita Ilaria <ilaria.colavita@studio.unibo.it>
2024-02-13 04:22:39 +0100

    Add dark colors

--- src/main/java/utility/BColor.java
+++ src/main/java/utility/BColor.java
@@ -2,6 +2,16 @@ package utility;
 
 /**
  * Board Colors.
+ * @see BLUE
+ * @see RED
+ * @see GREEN
+ * @see YELLOW
+ * @see DARK_BLUE
+ * @see DARK_RED
+ * @see DARK_GREEN
+ * @see DARK_YELLOW
+ * @see GREY
+ * @see DARK_GREY
  */
 public enum BColor {
     /**
@@ -21,9 +31,29 @@ public enum BColor {
      */
     YELLOW("#fec00f"),
     /**
+     * Dark blue color.
+     */
+    DARK_BLUE("#2559ba"),
+    /**
+     * Dark red color.
+     */
+    DARK_RED("#c42e14"),
+    /**
+     * Dark green color.
+     */
+    DARK_GREEN("#0d6634"),
+    /**
+     * Dark yellow color.
+     */
+    DARK_YELLOW("#feaa0f"),
+    /**
      * Grey color, the default one.
      */
-    GREY("#f1f1f1");
+    GREY("#f1f1f1"),
+    /**
+     * Dark grey.
+     */
+    DARK_GREY("#363230");
 
     private String hexColor;
 
